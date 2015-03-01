512dab0f3fe304575b5ba49b049e622436031255
Shelby <vanhooser@ou.edu>
2015-03-01 17:23:30 -0600

    Updated add and update routes

--- app.js
+++ app.js
@@ -1,59 +1,44 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express')
+  , routes = require('./routes');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/live');
 
-var app = express();
+var app = module.exports = express.createServer();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// Configuration
 
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.set('view options', {
+    layout: false
+  });
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(function(req,res,next){
+    req.db = db;
+    next();
+  });
+  app.use(express.static(__dirname + '/public'));
 });
 
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
 
-module.exports = app;
+// Routes
+
+app.post('/user/add', routes.addUser);
+app.get('/user/update', routes.updateUser);
+
+
+app.listen(1000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,51 @@
-var express = require('express');
-var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
+/*
+ * GET home page.
+ */
 
-module.exports = router;
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/live');
+
+exports.addUser = function(req, res) {
+  var collection = db.get('users'); 
+
+  var newUser = {
+      firstName : req.body.firstName,
+      lastName : req.body.lastName,
+      cell : req.body.cell
+  };
+
+  collection.insert(newUser, {}, function(err, records){
+    if(err){
+      res.end('{"error" : "something went terribly wrong", "status" : 500}');
+    } else {
+      res.end('{"success" : "Updated Successfully", "status" : 200}');
+    }
+  });
+};
+
+exports.updateUser = function(req, res) {
+  var collection = db.get('users');
+
+  var user = {
+    _id : req.body._id,
+    firstName : req.body.firstName,
+    lastName : req.body.lastName,
+    cell : req.body.cell
+  }
+
+  collection.update({_id : req.body._id}, user, {}, function(err, records){
+    if(err){
+      res.json(err);
+      res.end();
+    } else {
+      res.json(records);
+      res.end();
+    }
+  });
+}
+
+exports.index = function(req, res){
+  res.render('index')
+};
